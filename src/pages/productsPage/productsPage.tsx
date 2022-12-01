3c6d41379909cbfa27753ae4301b1d361f587944
Admin <admin@Admins-MacBook-Pro.local>
2022-12-01 03:41:22 -0800

    create departments page

--- src/pages/productsPage/productsPage.tsx
+++ src/pages/productsPage/productsPage.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { fetchProducts } from './productsPageSlice'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { useParams } from 'react-router'
+import { useEffect } from 'react'
 
+import './styles.css'
 const ProductsPage = () => {
+  const { categoryId } = useParams()
+  const dispatch = useAppDispatch()
+  const products = useAppSelector((state) => state.products.products)
+  const productsStatus = useAppSelector((state) => state.products.status)
+  const eror = useAppSelector((state) => state.products.error)
+
+  useEffect(() => {
+    if (productsStatus === 'idle' || productsStatus === 'succeeded') {
+      dispatch(fetchProducts(categoryId as string))
+    }
+  }, [productsStatus, dispatch, categoryId])
+
   return (
-    <div>ProductsPage</div>
+    <div className='products-container'>
+      {products.map((product) => {
+        return (
+          <div
+            key={product.id}
+            className='product'
+          >
+            <img
+              key={product.imageUrls[0]}
+              src={product.imageUrls[0]}
+            />
+            <p className='product-title'>{product.title}</p>
+            <p>{'$' + product.price}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
