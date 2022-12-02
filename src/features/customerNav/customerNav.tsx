e32a56ace126b526de26aae6a588f9e55d006a10
Tatev Melikyan <tatev.melikyan@ymail.com>
2022-12-02 15:35:07 +0400

    Add cart dropdown.

--- src/features/customerNav/customerNav.tsx
+++ src/features/customerNav/customerNav.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 import { Link } from 'react-router-dom'
 
 import { FaUserAlt, FaHeart } from 'react-icons/fa'
 import { HiShoppingCart } from 'react-icons/hi'
+import CartDropdown from './cartDropdown'
+
+
 
 const CustomerNav = () => {
+  const [hideCartDropDown, setHideCartDropDown] = useState(true)
+
+  const handleCartHover = () => {
+    setHideCartDropDown(prevState => !prevState)
+  }
+
+
   return (
     <nav className='customer-nav'>
       <ul>
@@ -19,12 +29,15 @@ const CustomerNav = () => {
             <FaHeart />
           </Link>
         </li>
-        <li>
+        <li className='cart-link' onMouseOver={handleCartHover} onMouseOut={handleCartHover}>
           <Link to={'/shoppingcart'}>
             <HiShoppingCart />
           </Link>
         </li>
       </ul>
+      {
+        !hideCartDropDown && <CartDropdown /> 
+      }
     </nav>
   )
 }
