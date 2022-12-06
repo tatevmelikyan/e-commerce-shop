e525c69a92ce47a65faa1472230f43834f31a0a4
Tatev Melikyan <tatev.melikyan@ymail.com>
2022-12-06 16:07:19 +0400

    Add main video to homePage

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
+//@ts-ignore
+import vid from '../../assets/pexels-tima-miroshnichenko-5834599.mp4'
+import './styles.css'
+
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  const navigate = useNavigate()
+  return <div>
+<div className='main-video-container'>
+  <video src={vid} muted autoPlay loop></video>
+  <div className='video-ad-container'>
+    <div className='text-shop'>
+      <span>Bring The Christmas <br /> Spirit To Your Home</span>
+      <button onClick={() => navigate('/holidays')}>SHOP NOW</button>
+    </div>
+  </div>
+ 
+</div>
+  </div>
 }
 
 export default HomePage
--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const LoginPage = () => {
   return (
     <div>LoginPage</div>
