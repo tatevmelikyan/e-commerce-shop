7220b44f8ad24b9c27f017875ebece035293b7a3
Zhanna <zhannagrigoryan90@gmail.com>
2022-12-12 17:40:30 +0400

    favorite page initial  commit

--- src/pages/favorites/favoritesPage.tsx
+++ src/pages/favorites/favoritesPage.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { IProduct } from '../productPage/productPage'
 
 const FavoritesPage = () => {
+
+  const [favorites, setFavorites] = useState<IProduct[]>()
+
+  useEffect(()=>{
+    const json = localStorage.getItem('favorites')
+    if (json){
+      setFavorites(JSON.parse(json))
+    }
+    console.log(favorites);
+    
+  },[])
+
   return (
     <div>FavoritesPage</div>
   )
