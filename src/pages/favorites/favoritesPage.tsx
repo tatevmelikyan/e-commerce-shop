326e0c457f3d35b7348827b6f61521133973baac
Zhanna <zhannagrigoryan90@gmail.com>
2022-12-13 17:14:53 +0400

    display message when there are no favorite items

--- src/pages/favorites/favoritesPage.tsx
+++ src/pages/favorites/favoritesPage.tsx
@@ -5,51 +5,38 @@ import { IProduct } from '../productPage/productPage'
 import ProductsUI from '../products/productsUI'
 import { getLikedProducts } from './favoritesSlice'
 import LikeIcon from './likeIcon'
+import NoFavorites from './noFavorites'
 import './styles.css'
 
 const FavoritesPage = () => {
 
-  // const [favorites, setFavorites] = useState<IProduct[]>()
+ 
  const dispatch = useAppDispatch()
  const favorites = useAppSelector(state=>state.favoriteItems.favoriteItems)
  
   useEffect(()=>{
-    // const json = localStorage.getItem('favorites')
-    // if (json){
-    //   setFavorites(JSON.parse(json))
-    // } 
+    
     dispatch(getLikedProducts())
   },[])
 
 
   return (
-    <div className='favoritePage'>
-     <div className='favoriteHeader'>
-      <h1 >Your Favorite Items</h1>
-      <p>Item count: { favorites?.length}</p>
-     </div>
-     <div className='favoritesContainer'>
-     <ProductsUI products={favorites}/>
-     </div>
-     
-
-    {/* <div className='favoritesContainer'>
-   
+    <>
     {
-      favorites?.map(item=> <div className='favoritePageItem' key={item.id}>
-        <div className='favorite-image-container'>
-          <img src={item.imageUrls[0]} alt="" className='itemImage'/>
-         <div  className='favoritePageLikeIcon'>
-           <LikeIcon product={item}/>
-         </div>
-        </div>
-        <p>{item.title}</p>
-        <p>{item.price}</p>
-         </div>)
+       favorites.length?
+       <div className='favoritePage'>
+       <div className='favoriteHeader'>
+       <h1 >Your Favorite Items</h1>
+       <p>Item count: { favorites?.length}</p>
+      </div>
+      <div className='favoritesContainer'>
+      <ProductsUI products={favorites}/>
+      </div>
+      </div>
+      :
+      <NoFavorites/>
     }
-  </div> */}
-</div>
-    
+    </>
   )
 }
 
