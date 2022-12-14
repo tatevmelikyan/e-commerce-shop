f815bd24d5e3062591868690d827194c83097d86
Atajanyan <atajanyanh@gmail.com>
2022-12-14 18:13:20 +0400

    add zoom feature in product page image

--- src/pages/productPage/productPageCarusel.tsx
+++ src/pages/productPage/productPageCarusel.tsx
@@ -6,6 +6,8 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import { IProduct } from './productPage'
 import LikeIcon from '../favorites/likeIcon'
+import { TransformWrapper,TransformComponent } from 'react-zoom-pan-pinch'
+
 
 const ProductPageCarusel:React.FC<{slideContent:IProduct}> = ({slideContent}) => {
   return (
@@ -22,10 +24,15 @@ const ProductPageCarusel:React.FC<{slideContent:IProduct}> = ({slideContent}) =>
           return (
             <SwiperSlide key={item}>
               <div className='slide-item-productePage'>
+                <TransformWrapper>
+                  <TransformComponent>
                   <img
+                    className='productPageImg'
                     src={item}
                     alt='product'
                   />
+                    </TransformComponent>
+                  </TransformWrapper>
                      <div className='like-icon-container'>
                 <LikeIcon product={slideContent as IProduct} />
               </div>
