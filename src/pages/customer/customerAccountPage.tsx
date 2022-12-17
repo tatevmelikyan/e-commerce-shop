0bf3ad737f14131a5d3fed5074c55cee3d1da967
Tatev Melikyan <tatev.melikyan@ymail.com>
2022-12-17 23:29:10 +0400

    Changed brand name.

--- src/pages/customer/customerAccountPage.tsx
+++ src/pages/customer/customerAccountPage.tsx
@@ -1,17 +1,10 @@
-import { onAuthStateChanged } from 'firebase/auth'
-import React, {useEffect, useState} from 'react'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { setCurrentUser } from '../../features/slices/currentUserSlice'
-import { auth } from '../../firebase/auth'
+import React from 'react'
+import { useAppSelector } from '../../app/hooks'
 
 const CustomerAccountPage = () => {
-const currentUser = useAppSelector(state => state.currentUser)
+  const currentUser = useAppSelector((state) => state.currentUser)
 
-// console.log(currentUser, 'user in store');
-  
-  return (
-    <div>Welcome {currentUser.name}</div>
-  )
+  return <div>Welcome {currentUser.name}</div>
 }
 
 export default CustomerAccountPage
