9c3e00630b01afa241c911694c4e470e0eecdcb2
Atajanyan <atajanyanh@gmail.com>
2022-12-18 12:52:46 +0400

    fix product page animation

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import './styles.css'
+import './headerMedia.css'
 
 import { Link } from 'react-router-dom'
 import CustomerNav from '../customerNav/customerNav'
 import DepartmentsNav from '../departmentsNav/departmentsNav'
 import SearchField from '../search/searchField'
+import { BsSearch } from 'react-icons/bs'
 
-const Header = () => {
+const Header = () => {  
   return (
     <div className='sticky-header'>
       <header>
         <div className='search-logo-customer'>
+          <div className='search-div-container'>
           <SearchField />
+          </div>
+          <div className='search-media'>
+          <BsSearch/>
+          </div>
           <div className='brand-logo'>
             <Link to='/'>SAVILLA</Link>
           </div>
