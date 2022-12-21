8242e9ea8165716551c0ca50dca7c0f5df16da89
Atajanyan <atajanyanh@gmail.com>
2022-12-21 15:05:31 +0400

    response products page

--- src/pages/department/departmentPage.tsx
+++ src/pages/department/departmentPage.tsx
@@ -1,4 +1,5 @@
 import './styles.css'
+import './departmentMedia.css'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
